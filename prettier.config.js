e3c4cb79a40137219751f726b7c53c3132331458
k-ueda <k_ueda@waku-2.com>
2023-11-29 08:40:46 +0900

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// const organizeImports = require('prettier-plugin-organize-imports');
+
+module.exports = {
+  plugins: ['prettier-plugin-organize-imports'],
+  organizeImportsSkipDestructiveCodeActions: true,
+  printWidth: 140,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+};
