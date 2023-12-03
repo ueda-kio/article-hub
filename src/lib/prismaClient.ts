db939fc13e6083aca31906094be4cb9fa223c73f
k-ueda <k_ueda@waku-2.com>
2023-12-03 20:57:31 +0900

    refactor prismaClient

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -1,4 +1,18 @@
-// /lib/prisma.ts
+declare global {
+  var prisma: PrismaClient;
+}
+
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export { prisma };
