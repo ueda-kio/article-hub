b354c1380d34d516f56c7f64dd8577d6c28ce51c
k-ueda <k_ueda@waku-2.com>
2023-12-07 17:02:30 +0900

    add branch myPage

--- src/app/member/[uid]/page.tsx
+++ src/app/member/[uid]/page.tsx
@@ -1,15 +1,18 @@
+import { getServerSession } from '@/auth';
 import { Article, User } from '@prisma/client';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 import ArticleList from './ArticleList';
 import TextArea from './TextArea';
 
-const getUser = async () => {
+const getUser = async (uid: string) => {
   const res = await fetch('http://localhost:3000/api/user', {
     next: {
       tags: ['users'],
     },
   });
   const json = await res.json();
-  return json.users as User[];
+  return (json.users as User[]).find((user) => user.id === uid);
 };
 
 const getArticles = async (uid: string) => {
@@ -22,24 +25,48 @@ const getArticles = async (uid: string) => {
   return json.articles as Article[];
 };
 
+const getSessionId = async () => {
+  const session = await getServerSession();
+  return session?.user.id;
+};
+
 export default async function UserDetail({ params }: { params: { uid: string } }) {
-  const users = await getUser();
-  const articles = await getArticles(params.uid);
-  const user = users.find((user) => user.id === params.uid);
-  if (!user) return <p>user not found</p>;
+  const uid = params.uid;
+  const [user, articles, sessionId] = await Promise.all([getUser(uid), getArticles(uid), getSessionId()]);
+  const isMyPage = uid === sessionId;
+  if (!user) {
+    notFound();
+  }
+
   return (
     <>
-      <h1 className="text-2xl font-bold">User Detail</h1>
-      <p>welcome {user.name} !</p>
+      <h1 className="text-2xl font-bold">{isMyPage ? `おかえり！${user.name}` : `${user.name}のページだよ`}</h1>
       <br />
-      <section>
-        <TextArea id={user.id} qiita={user.qiita} zenn={user.zenn} />
-      </section>
-      <br />
-      <section>
-        <h2 className="text-xl font-semibold">Articles</h2>
-        <ArticleList fallback={articles} uid={user.id} />
-      </section>
+      {isMyPage ? (
+        <>
+          <section>
+            <TextArea id={user.id} qiita={user.qiita} zenn={user.zenn} />
+          </section>
+          <br />
+          <section>
+            <h2 className="text-xl font-semibold">Articles</h2>
+            <ArticleList fallback={articles} uid={user.id} />
+          </section>
+        </>
+      ) : (
+        <>
+          {articles.map(
+            (article) =>
+              article.publish && (
+                <li key={article.id}>
+                  <Link href={article.url} className="text-blue-500 hover:underline" target="_blank" passHref>
+                    {article.title}
+                  </Link>
+                </li>
+              ),
+          )}
+        </>
+      )}
     </>
   );
 }
