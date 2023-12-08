32d32539fe133dff8cf706d3fd511f97d814ebee
k-ueda <k_ueda@waku-2.com>
2023-12-08 10:58:42 +0900

    set baseUrl

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_API_URL : 'http://localhost:3000';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_API_URL : 'http://localhost:3000';
+// export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
--- src/lib/getResources.ts
+++ src/lib/getResources.ts
@@ -4,15 +4,27 @@ import { BASE_URL } from './baseUrl';
 export const getUsers = async (uid?: string) => {
   const url = `${BASE_URL}/api/user${uid ? `?id=${uid}` : ''}`;
   const tag = uid ? `user-${uid}` : 'users';
-  const res = await fetch(url, { next: { tags: [tag] } });
-  const json = await res.json();
-  return json.users as User[];
+  try {
+    const res = await fetch(url, { next: { tags: [tag] } });
+    const json = await res.json();
+    // console.log('users: ', json);
+    return json.users as User[];
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 };
 
 export const getArticles = async (creatorId?: string) => {
   const url = `${BASE_URL}/api/article${creatorId ? `?creatorId=${creatorId}` : ''}`;
   const tag = creatorId ? `articles-${creatorId}` : 'articles';
-  const res = await fetch(url, { next: { tags: [tag] } });
-  const json = await res.json();
-  return json.articles as Article[];
+  try {
+    const res = await fetch(url, { next: { tags: [tag] } });
+    const json = await res.json();
+    // console.log('articles: ', json);
+    return json.articles as Article[];
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 };
